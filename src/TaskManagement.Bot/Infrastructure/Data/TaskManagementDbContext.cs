6aaca000cbd0ab4be3060b7740a2b8962eaec349
MinNghiax <tonghia2004pm@gmail.com>
2026-04-09 10:29:40 +0700

    refactor(db): unify database configuration using DesignTimeDbContextFactory

--- src/TaskManagement.Bot/Infrastructure/Data/TaskManagementDbContext.cs
+++ src/TaskManagement.Bot/Infrastructure/Data/TaskManagementDbContext.cs
@@ -1,31 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using TaskManagement.Bot.Infrastructure.Entities;
-using TaskManagement.Bot.Infrastructure.Configurations;
 
 namespace TaskManagement.Bot.Infrastructure.Data
 {
     public class TaskManagementDbContext : DbContext
     {
-        public TaskManagementDbContext(DbContextOptions<TaskManagementDbContext> options) : base(options)
+        public TaskManagementDbContext(DbContextOptions<TaskManagementDbContext> options)
+            : base(options)
         {
         }
 
         public DbSet<TaskItem> TaskItems { get; set; }
-
         public DbSet<TaskClan> TaskClans { get; set; }
-
         public DbSet<TaskThread> TaskThreads { get; set; }
-
         public DbSet<Reminder> Reminders { get; set; }
-
         public DbSet<ReminderRule> ReminderRules { get; set; }
-
         public DbSet<Complain> Complains { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(TaskManagementDbContext).Assembly);
         }
     }
